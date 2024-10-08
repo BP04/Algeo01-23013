d87337aa992b1d5744adae63b35ed00b411b1164
Nathaniel Jonathan Rusli <163715460+0xNathaniel@users.noreply.github.com>
2024-10-08 12:46:35 +0700

    feat: add gauss jordan elimination func

---
+++ src/GaussJordanElimination.java
@@ -0,0 +1,20 @@
+public class GaussJordanElimination {
+    public static Matrix gauss_jordan_elimination(Matrix matrix){
+        int cols = matrix.get_cols();
+        //Execute gaussian elimination
+        matrix = GaussianElimination.gaussian_elimination(matrix);
+
+        //Execute backward elimination
+        for(int i = 1; i < cols; i++){
+            for(int j = 0; j < i; j++){
+                if (matrix.get(j, i) != 0) {
+                    double constants = (matrix.get(j, i) / matrix.get(i, i));
+                    for(int k = 0; k < cols; k++) {
+                        matrix.set(j, k, (matrix.get(j, k) - matrix.get(i, k) * constants));
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+}
