943213d342e5b9714dc4f005fe3f00dbca47a980
Nathaniel Jonathan Rusli <163715460+0xNathaniel@users.noreply.github.com>
2024-10-09 15:22:58 +0700

    fix: change looping boundaries

--- src/GaussJordanElimination.java
+++ src/GaussJordanElimination.java
@@ -1,11 +1,11 @@
 public class GaussJordanElimination {
     public static Matrix gauss_jordan_elimination(Matrix matrix){
-        int cols = matrix.get_cols();
+        int rows = matrix.get_rows(), cols = matrix.get_cols();
         //Execute gaussian elimination
         matrix = GaussianElimination.gaussian_elimination(matrix);
 
         //Execute backward elimination
-        for(int i = 1; i < cols; i++){
+        for(int i = 1; i < rows; i++){
             for(int j = 0; j < i; j++){
                 if (matrix.get(j, i) != 0) {
                     double constants = (matrix.get(j, i) / matrix.get(i, i));
