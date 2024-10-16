bd83ff23a8143e96c79ec66336ee4f81f2ca981b
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-10-17 01:00:51 +0700

    fix: add row normalization to gauss jordan elimination, add parametric output to spl gauss jordan, and recompile inverse matrix gauss jordan

--- src/SPLGaussJordan.java
+++ src/SPLGaussJordan.java
@@ -4,40 +4,90 @@ public class SPLGaussJordan {
 
         // Execute the Gauss-Jordan Elimination function on matrix
         matrix = GaussJordanElimination.gauss_jordan_elimination(matrix);
+        matrix.print_matrix();
 
         boolean has_contradiction = false;
         boolean has_all_zeros = false;
+        
         // Infinite solutions and no solution case checking
         for (int i = 0; i < rows; i++) {
-            double sum = 0;
+            boolean is_all_zero = true;
             for (int j = 0; j < cols - 1; j++) {
-                sum += matrix.get(i, j);
+                if (matrix.get(i, j) != 0) {
+                    is_all_zero = false;
+                }
             }
             // Case: infinite solutions (has at least one row of zeros)
-            if ((sum == 0) && (matrix.get(i, cols-1) == 0)) {
+            if ((is_all_zero) && (matrix.get(i, cols - 1) == 0)) {
                 has_all_zeros = true;
-            }   
+            }
             // Case: no solution (has at least one row of contradiction, i.e. all zeros except for the last column)
-            if ((sum == 0) && (matrix.get(i, cols-1) != 0)) {
+            if ((is_all_zero) && (matrix.get(i, cols - 1) != 0)) {
                 has_contradiction = true;
-            }   
+            }
         }
 
         // Calculation output
         if (has_contradiction) {
             System.out.println("Matrix does not have a solution.");
-        }
-        else if ((rows < (cols-1)) || (has_all_zeros)) {
+        } 
+        else if ((rows < (cols - 1)) || has_all_zeros) {
             System.out.println("Matrix has infinite solutions.");
-        }
+            System.out.println("Parametric solution:");
+
+            boolean[] is_free_variable = new boolean[cols - 1];
+            
+            // Search for free variables
+            for (int j = 0; j < cols - 1; j++) {
+                boolean has_leading_one = false;
+                for (int i = 0; i < rows; i++) {
+                    if (matrix.get(i, j) == 1) {
+                        // Check if there's a already a leading one in the same column
+                        boolean is_leading_one = true;
+                        for (int k = 0; k < j; k++) {
+                            if (matrix.get(i, k) == 1) {
+                                is_leading_one = false;
+                            }
+                        }
+                        if (is_leading_one) {
+                            has_leading_one = true;
+                            break;
+                        }
+                    }
+                }
+                if (!has_leading_one) {
+                    is_free_variable[j] = true;
+                }
+            }
+
+            // Output the parametric solutions
+            for (int i = 0; i < cols - 1; i++) {
+                if (is_free_variable[i]) {
+                    System.out.println("x" + (i + 1) + " = t" + (i + 1));  
+                } 
+                else {
+                    System.out.print("x" + (i + 1) + " = ");
+                    if (matrix.get(i, cols - 1) != 0) {
+                        System.out.print(matrix.get(i, cols - 1));
+                    }
+                    for (int j = 0; j < cols - 1; j++) {
+                        if (is_free_variable[j] && matrix.get(i, j) != 0) {
+                            System.out.print(" - ");
+                            System.out.print(matrix.get(i, j) + " * t" + (j + 1));
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+        } 
         else {
             for (int i = 0; i < rows; i++) {
-                System.out.print("x" + (i+1) + " = " + matrix.get(i, cols-1));
+                System.out.print("x" + (i + 1) + " = " + matrix.get(i, cols - 1));
                 if (i < rows - 1) {
                     System.out.print(", ");
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
