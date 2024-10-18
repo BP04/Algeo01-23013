8205651414b77b9d0bfa1412e78c8a238c07191a
Naomi Risaka <163662184+naomirisaka@users.noreply.github.com>
2024-10-18 12:41:08 +0700

    feat: add parametric solution

--- src/SPLInverse.java
+++ src/SPLInverse.java
@@ -4,7 +4,7 @@ public class SPLInverse {
         int cols = matrix.get_cols();
         
         if (cols != rows + 1) {
-            throw new IllegalArgumentException("Matrix is not square.");
+            throw new IllegalArgumentException("Matrix is not augmented (last column should be constants).");
         }
 
         Matrix coefficients = new Matrix(rows, rows);
@@ -13,38 +13,18 @@ public class SPLInverse {
             for (int j = 0; j < rows; j++) {
                 coefficients.set(i, j, matrix.get(i, j));
             }
-            constants.set(i, 0, matrix.get(i, cols-1));
+            constants.set(i, 0, matrix.get(i, cols - 1));
         }
 
         double det = DeterminantRowReduction.determinant_row_reduction(coefficients);
         if (det == 0) {
-            matrix = GaussJordanElimination.gauss_jordan_elimination(matrix);
-            boolean has_contradiction = false;
-            boolean has_all_zeros = false;
-            for (int i = 0; i < rows; i++) {
-                double sum = 0;
-                for (int j = 0; j < cols-1; j++) {
-                    sum += matrix.get(i, j); 
-                }
-                if (sum == 0 && matrix.get(i, cols - 1) == 0) {
-                    has_all_zeros = true;  
-                }
-                if (sum == 0 && matrix.get(i, cols - 1) != 0) {
-                    has_contradiction = true;  
-                }
-            }
-            if (has_contradiction) {
-                System.out.println("Matrix does not have a solution.");
-            } else if (has_all_zeros) {
-                System.out.println("Matrix has infinite solutions.");
-            }
-
+            SPLGaussian.spl_gaussian(matrix);
         } else {
             Matrix inverse = InverseMatrixGaussJordan.inverse_matrix(coefficients);
             Matrix solution = inverse.multiply(constants);
             for (int i = 0; i < rows; i++) {
                 System.out.print("x" + (i + 1) + " = " + solution.get(i, 0));
-                if (i < rows-1) {
+                if (i < rows - 1) {
                     System.out.print(", ");
                 }
             }
