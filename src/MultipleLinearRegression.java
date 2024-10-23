b37fc54f0c617b8252a3bf63e384cbba21a3c622
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-10-23 21:27:10 +0700

    feat: add xk input

--- src/MultipleLinearRegression.java
+++ src/MultipleLinearRegression.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
 public class MultipleLinearRegression {
-    public static void multiple_linear_regression(Matrix matrix) {
-        int rows = matrix.get_rows();
-        int cols = matrix.get_cols() - 1;
+    public static void multiple_linear_regression(Matrix matrix, Matrix xk) {
+        int rows = matrix.get_rows(), cols = matrix.get_cols() - 1;
+        int rows2 = xk.get_rows(), cols2 = xk.get_cols();
+
+        if (cols <= 0) {
+            throw new IllegalArgumentException("Insufficient data features");
+        }
+
+        // matrix and xk dimension  checking
+        if (cols != cols2) {
+            throw new IllegalArgumentException("Mismatch of input matrix and xk's number of features");
+        }
 
         Matrix x = new Matrix(rows, cols + 1);
         Matrix y = new Matrix(rows, 1);
 
+        // matrix columns expansion
         for (int i = 0; i < rows; i++) {
             x.set(i, 0, 1);  
             for (int j = 1; j <= cols; j++) {
@@ -15,6 +25,16 @@ public class MultipleLinearRegression {
             }
             y.set(i, 0, matrix.get(i, cols));
         }
+
+        Matrix pred_matrix = new Matrix(rows2, cols2 + 1);
+        for (int i = 0; i < rows2; i++) {
+            pred_matrix.set(i, 0, 1);  
+            for (int j = 1; j <= cols2; j++) {
+                pred_matrix.set(i, j, xk.get(i, j - 1));
+            }
+        }
+
+        // xk columsn expansion
 
         // Regularization's lambda initialization
         double lambda = 0.0;
@@ -80,29 +100,25 @@ public class MultipleLinearRegression {
 
         // Output terms result
         System.out.println();
-        System.out.println("Regression terms:");
-        System.out.println("c" + " = " + beta.get(0, 0));
+        System.out.print("f(x) = ");
+        System.out.print(beta.get(0, 0) + " + ");
         for (int i = 1; i < beta.get_rows(); i++) {
-            System.out.println("x" + (i) + " = " + beta.get(i, 0));
+            System.out.print(beta.get(i, 0) + "x" + i);
+            if (i < beta.get_rows() - 1) {
+                System.out.print(" + ");
+            }
         }
 
         // Output predictions
         System.out.println();
-        Matrix predictions = x.multiply(beta);
-        System.out.println("Input predictions: ");
-        for (int i = 0; i < rows; i++) {
-            System.out.print((i+1) + ". f" + (i+1) + "(");
-            System.out.print("c, ");
-            for (int j = 1; j < beta.get_rows(); j++) {
-                System.out.print("x" + (j));
-                if (j < beta.get_rows() - 1) {
-                    System.out.print(", ");
-                }
-                else {
-                    System.out.print(") = ");
-                }
-            }
-            System.out.println(predictions.get(i, 0));
+        Matrix predictions = pred_matrix.multiply(beta);
+        if (rows2 == 1) {
+            System.out.println("f(xk) = " + predictions.get(0, 0));
+        }
+        else {
+            for (int i = 0; i < rows2; i++) {
+                System.out.println("f(xk" + (i+1) + ") = " + predictions.get(i, 0));
+            } 
         }
     }
 }
