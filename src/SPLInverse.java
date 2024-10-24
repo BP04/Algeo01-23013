bd47ab281a37f374fc709008b046657574fd843b
Naomi Risaka <163662184+naomirisaka@users.noreply.github.com>
2024-10-24 18:04:31 +0700

    feat: save output to file

--- src/SPLInverse.java
+++ src/SPLInverse.java
@@ -1,111 +1,105 @@
-public class SPLInverse {
-    private static void swap_rows(Matrix matrix, int row1, int row2) {
-        double[] temp = matrix.copy_row(row1);
-        int cols = matrix.get_cols();
-        for(int j = 0; j < cols; ++j){
-            matrix.set(row1, j, matrix.get(row2, j));
-        }
-        for(int j = 0; j < cols; ++j){
-            matrix.set(row2, j, temp[j]);
-        }
-    }
-
-    public static Matrix basis(Matrix matrix){
+import java.util.Scanner;
+public class SPLGaussJordan {
+    public static void spl_gauss_jordan(Matrix matrix) {
+        Scanner scanner = new Scanner(System.in);
         int rows = matrix.get_rows(), cols = matrix.get_cols();
-        Matrix temp = matrix.copy_matrix();
-        int[] rows_position = new int[rows];
 
-        for(int i = 0; i < rows; ++i){
-            rows_position[i] = i;
+        // Execute the Gauss-Jordan Elimination function on matrix
+        matrix = GaussJordanElimination.gauss_jordan_elimination(matrix);
+
+        boolean has_contradiction = false;
+        boolean has_all_zeros = false;
+        
+        // Infinite solutions and no solution case checking
+        for (int i = 0; i < Math.min(rows, cols - 1); i++) {
+            boolean is_all_zero = true;
+            for (int j = 0; j < cols - 1; j++) {
+                if (matrix.get(i, j) != 0) {
+                    is_all_zero = false;
+                }
+            }
+            // Case: infinite solutions (has at least one row of zeros)
+            if ((is_all_zero) && (matrix.get(i, cols - 1) == 0)) {
+                has_all_zeros = true;
+            }
+            // Case: no solution (has at least one row of contradiction, i.e. all zeros except for the last column)
+            if ((is_all_zero) && (matrix.get(i, cols - 1) != 0)) {
+                has_contradiction = true;
+            }
         }
 
-        for(int i = 0; i < rows; ++i){
-            if(temp.get(i, i) == 0){
-                for(int j = i + 1; j < rows; ++j){
-                    if(temp.get(j, i) != 0){
-                        swap_rows(temp, i, j);
-                        int aux = rows_position[i];
-                        rows_position[i] = rows_position[j];
-                        rows_position[j] = aux;
-                        break;
+        // Calculation output
+        if (has_contradiction) {
+            System.out.println("Matrix does not have a solution.");
+        } 
+        else if ((rows < (cols - 1)) || has_all_zeros) {
+            System.out.println("Matrix has infinite solutions.");
+            System.out.println("Parametric solution:");
+
+            boolean[] is_free_variable = new boolean[cols - 1];
+            
+            // Search for free variables
+            int not_free = 0;
+            for (int j = 0; j < cols - 1; j++) {
+                boolean has_leading_one = false;
+                for (int i = 0; i < rows; i++) {
+                    if (matrix.get(i, j) == 1) {
+                        // Check if there's a already a leading one in the same column
+                        boolean is_leading_one = true;
+                        for (int k = 0; k < j; k++) {
+                            if (matrix.get(i, k) == 1) {
+                                is_leading_one = false;
+                            }
+                        }
+                        if (is_leading_one) {
+                            has_leading_one = true;
+                            break;
+                        }
                     }
                 }
-            }
-
-            double pivot = temp.get(i, i);
-            if(pivot != 0){
-                for(int j = i; j < cols; ++j){
-                    temp.set(i, j, temp.get(i, j) / pivot);
+                if (!has_leading_one) {
+                    is_free_variable[j] = true;
+                }
+                else{
+                    not_free = j;
                 }
             }
 
-            for(int k = i + 1; k < rows; ++k){
-                double constant = temp.get(k, i);
-                for(int j = i; j < cols; ++j){
-                    temp.set(k, j, temp.get(k, j) - temp.get(i, j) * constant);
+            // Output the parametric solutions
+            int free_index = 1;
+            for (int i = 0; i < cols - 1; i++) {
+                if (is_free_variable[i]) {
+                    System.out.println("x" + (i + 1) + " = t" + (free_index));
+                    free_index++;  
+                } 
+                else {
+                    System.out.print("x" + (i + 1) + " = ");
+                    for(int j = not_free + 1; j < cols - 1; ++j){
+                        if(matrix.get(i, j) != 0){
+                            System.out.print(-matrix.get(i, j) + "t" + (j - not_free) + " + ");
+                        }
+                    }
+                    if(matrix.get(i, cols - 1) == 0){
+                        System.out.print("0.0");
+                    }
+                    else{
+                        System.out.print(matrix.get(i, cols - 1));
+                    }
+                    System.out.println();
                 }
             }
-        }
-
-        int used = -1;
-        for(int i = 0; i < rows; ++i){
-            boolean leading_one = false;
-            for(int j = 0; j < cols; ++j){
-                if(temp.get(i, j) == 1){
-                    leading_one = true;
-                    break;
-                }
-            }
-            if(leading_one){
-                used = i;
-            }
-        }
-
-        Matrix result = new Matrix(used + 1, cols);
-
-        for(int i = 0; i <= used; ++i){
-            int index = rows_position[i];
-            for(int j = 0; j < cols; ++j){
-                result.set(i, j, matrix.get(index, j));
-            }
-        }
-
-        return result;
-    }
-    
-    public static void spl_inverse(Matrix matrix) {
-        matrix = basis(matrix);
-        
-        int rows = matrix.get_rows();
-        int cols = matrix.get_cols();
-
-        if (cols != rows + 1) {
-            SPLGaussian.spl_gaussian(matrix);
-            return;
-        }
-
-        Matrix coefficients = new Matrix(rows, rows);
-        Matrix constants = new Matrix(rows, 1);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                coefficients.set(i, j, matrix.get(i, j));
-            }
-            constants.set(i, 0, matrix.get(i, cols - 1));
-        }
-
-        double det = DeterminantRowReduction.determinant_row_reduction(coefficients);
-        if (det == 0) {
-            SPLGaussian.spl_gaussian(matrix);
-        } else {
-            Matrix inverse = InverseMatrixGaussJordan.inverse_matrix(coefficients);
-            Matrix solution = inverse.multiply(constants);
-            for (int i = 0; i < rows; i++) {
-                System.out.print("x" + (i + 1) + " = " + solution.get(i, 0));
-                if (i < rows - 1) {
+        } 
+        else {
+            Matrix solution = new Matrix(rows, 1);
+            for (int i = 0; i < cols - 1; i++) {
+                System.out.print("x" + (i + 1) + " = " + matrix.get(i, cols - 1));
+                solution.set(i, 0, matrix.get(i, cols - 1));
+                if (i < cols - 2) {
                     System.out.print(", ");
                 }
             }
             System.out.println();
+            Matrix.save_output_SPL(scanner, solution);
         }
     }
 }
